d755acaccb17d5bf7f44799273306b971fb55c90
Roman <kaboom.ua@gmail.com>
2017-02-19 19:01:26 +0200

    Update idGenerator.js

--- src/middlewares/idGenerator.js
+++ src/middlewares/idGenerator.js
@@ -4,12 +4,14 @@ export default store => next => action => {
     if(!action.entityName || !action.payload[action.entityName] || action.payload[action.entityName].id !== null)
         return next(action);
 
+	//через мидлвары будут проходить все экшины, суть в том, что делать их нужно максимально реюзабильными. Не завязывайся на один экшин
     if(action.entityName == COMMENT_ENTITY_NAME)
     {
         const {payload: {...newPayload}, ...newAction}  = action
         const {...comment} = newPayload[action.entityName]
         // const {...newPayload} = payload
         const {comments} = store.getState()
+        //достаточно плохая логика для генерации id, никакой гарантии уникальности. Лучше просто Math.random()
         comment.id = Math.max(...Object.keys(comments)) + 1
         newPayload[action.entityName] = comment
         newAction.payload = newPayload
